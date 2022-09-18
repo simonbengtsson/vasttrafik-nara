2514a392ff5d8ab539f323c0721d26b3d461c498
Simon Bengtsson <simonbengtsson@users.noreply.github.com>
2022-09-18 16:35:55 +0200

    Geolocator plugin setup

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -5,7 +5,6 @@
 // ignore_for_file: lines_longer_than_80_chars
 
 import 'package:flutter_keyboard_visibility_web/flutter_keyboard_visibility_web.dart';
-import 'package:location_web/location_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
@@ -13,7 +12,6 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
   FlutterKeyboardVisibilityPlugin.registerWith(registrar);
-  LocationWebPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
