31dec9c5dd3eca284e510253467e2810b48275a4
Simon Bengtsson <simonbengtsson@users.noreply.github.com>
2022-09-19 16:19:11 +0200

    Update location getting

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -3,34 +3,56 @@ import FlutterMacOS
 import CoreLocation
 
 class MainFlutterWindow: NSWindow, CLLocationManagerDelegate {
-  override func awakeFromNib() {
-    let flutterViewController = FlutterViewController.init()
-    let windowFrame = self.frame
-    self.contentViewController = flutterViewController
-    self.setFrame(windowFrame, display: true)
-
-    RegisterGeneratedPlugins(registry: flutterViewController)
     
-    askForLocationPermission()
-
-    super.awakeFromNib()
-  }
+    let locationManager = CLLocationManager()
+    
+    override func awakeFromNib() {
+        let flutterViewController = FlutterViewController()
+        let windowFrame = self.frame
+        self.contentViewController = flutterViewController
+        self.setFrame(windowFrame, display: true)
+        
+        RegisterGeneratedPlugins(registry: flutterViewController)
+        
+        super.awakeFromNib()
+        
+        locationManager.delegate = self
+        askForLocationPermission()
+    }
     
     func askForLocationPermission() {
-        let locationManager = CLLocationManager()
-            locationManager.delegate = self
-            locationManager.startUpdatingLocation()
         let status = locationManager.authorizationStatus
-        if status == .restricted || status == .denied {
-            print("Location Denied")
-            return
-        } else if status == .notDetermined {
+        if status == .notDetermined {
             locationManager.requestWhenInUseAuthorization()
-            print("Show ask for location")
-            return
-        } else if status == .authorized {
-            print("This should work?")
-            return
+        }
+        print("Authorization status \(getDescription(locationManager.authorizationStatus))")
+    }
+    
+    func getDescription(_ status: CLAuthorizationStatus) -> String {
+        switch status {
+        case .authorizedAlways: return "authorizedAlways"
+        case .authorized: return "authorized"
+        case .denied: return "denied"
+        case .notDetermined: return "notDetermined"
+        case .restricted: return "restricted"
+        @unknown default: return "unknown"
         }
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations.first?.coordinate.latitude ?? 0)
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        print("Authorization status changed \(getDescription(manager.authorizationStatus))")
+        if manager.authorizationStatus == .authorized {
+            manager.startUpdatingLocation()
+            print("Started updating location...")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location manager error \(error.localizedDescription)")
+    }
+    
 }
